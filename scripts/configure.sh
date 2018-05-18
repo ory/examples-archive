0be9751eeccb2897cb95e81c6ff0f1d5319c0059
arekkas <aeneas@ory.am>
2018-05-18 14:18:07 +0200

    Scripts now skip configuration if directory does not exists

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -10,18 +10,32 @@ source /scripts/helper/envsubstfiles.sh
 source /scripts/helper/getid.sh
 
 echo "Substituting environment variables"
-envsubstfiles "/config/hydra/clients/*"
-envsubstfiles "/config/keto/policies/*"
-envsubstfiles "/config/oathkeeper/rules/*"
+if [ -d "/config/hydra/clients/" ]; then
+    envsubstfiles "/config/hydra/clients/*.json"
+fi
+if [ -d "/config/keto/policies/" ]; then
+    envsubstfiles "/config/keto/policies/*.json"
+fi
+if [ -d "/config/oathkeeper/rules/" ]; then
+    envsubstfiles "/config/oathkeeper/rules/*.json"
+fi
 
 echo "Executing bootstrap scripts..."
 
-hydra_url=$HYDRA_URL
-oathkeeper_url=$OATHKEEPER_API_URL
-keto_url=$KETO_URL
+hydra_url=${HYDRA_URL:=undefined}
+oathkeeper_url=${OATHKEEPER_API_URL:=undefined}
+keto_url=${KETO_URL:=undefined}
 
 export HYDRA_URL=${hydra_url%/}/
 
-backoff /scripts/services/hydra.sh "/config/hydra"
-backoff /scripts/services/oathkeeper.sh ${oathkeeper_url%/}/ "/config/oathkeeper"
-backoff /scripts/services/keto.sh ${keto_url%/}/ "/config/keto"
+if [ -d "/config/hydra" ]; then
+    backoff /scripts/services/hydra.sh "/config/hydra"
+fi
+
+if [ -d "/config/oathkeeper" ]; then
+    backoff /scripts/services/oathkeeper.sh ${oathkeeper_url%/}/ "/config/oathkeeper"
+fi
+
+if [ -d "/config/keto" ]; then
+    backoff /scripts/services/keto.sh ${keto_url%/}/ "/config/keto"
+fi
