c3a9d1dcc3c6092fe6718cf734f5777afa5dde05
zecarlos94 <zecarlosbg94@gmail.com>
2019-03-06 10:52:55 +0000

    Added roles support

--- scripts/services/keto.sh
+++ scripts/services/keto.sh
@@ -6,6 +6,23 @@ source "$( dirname "${BASH_SOURCE[0]}" )/../helper/getid.sh"
 
 url=$1
 path=$2
+role_path="$path/roles/*.json"
+
+echo "Deleting roles in $role_path..."
+for filename in $role_path; do
+    id=$(getid $filename)
+    (set -x; keto engines acp ory roles delete --endpoint $url exact $id || true)
+done
+echo "Deleted all roles in $role_path!"
+
+echo "Importing roles in $role_path..."
+for filename in $role_path; do
+    (set -x; keto engines acp ory roles import --endpoint $url exact $filename)
+done
+echo "Imported all roles in $role_path!"
+
+url=$1
+path=$2
 policy_path="$path/policies/*.json"
 
 echo "Deleting policies in $policy_path..."
